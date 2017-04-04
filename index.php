0481e17382b2893afbb2d910d0fb4ae159b47cf3
pauma <parjoona@hotmail.com>
2017-04-04 21:59:33 +0200

    Added-beta-website

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pauma
+ * Date: 2017-03-30
+ * Time: 12:01
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>This is a title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="jsbin/script.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="cssbin/style.css">
+</head>
+<body>
+
+<div id="wrapper">
+    <header>
+        <h1>PROJECTNAME HERE</h1>
+        <img src="">
+        <!-- MAKE MENU CENTER  -->
+        <div id="outerTable">
+            <div class="table">
+                <div id="settingButton">
+                    <ul id="horizontalList">
+                        <li>Cathegory</li>
+                        <li>Place</li>
+                        <li>Age</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- MENU CENTER END -->
+    </header>
+
+    <main>
+
+    </main>
+</div>
+</body>
+</html>
