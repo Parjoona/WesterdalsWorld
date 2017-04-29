f94da52b6ba9ffa4f83842e4df5b80d0e2974944
pauma <parjoona@hotmail.com>
2017-04-29 22:53:14 +0200

    Rydda upp i kod.

--- add_event/add_event.php
+++ add_event/add_event.php
@@ -1,45 +1,32 @@
 <!DOCTYPE html>
 <html>
-
 <head>
-	<!-- la till link till css -->
-	<link rel="stylesheet" href="../cssbin/style.css" type="text/css" />
-	<style>
-
-
-	</style>
-
-
-	<title>Add Event</title>
+    <link rel="stylesheet" href="../cssbin/style.css" type="text/css"/>
+    <title>Add Event</title>
 </head>
 
 <body>
-	<!-- La till den php require + main taggen -->
-	<?php require '../header.php' ?>
-	<main id="content">
-		<div id="formdiv">
-
-			<form id="form" method="post" action="process.php">
-				<label for="event_name">Event name:</label>
-				<input type="text" name="event_name" placeholder="Navn på eventet" />
-				<label for="description">Description</label>:
-				<textarea name="description">Beskrivelse...</textarea>
-				<label for="cost">Cost:</label>
-				<input type="number" name="cost" placeholder="Hvor mye koster det?" />
-				<!--<label>Image</label>
-		<input type="image" name="image" />
-		<br>-->
-				<label for="date_time">When:</label>
-				<input type="datetime-local" name="date_time" />
-				<label for="venue">Venue:</label>
-				<input type="text" name="venue" placeholder="Utested" />
-				<input type="submit" value="Add event">
-			</form>
-		</div>
-
-	</main>
-
-
+<!-- La till den php require + main taggen -->
+<?php require '../header.php' ?>
+<main id="content">
+    <div id="formdiv">
+        <form id="form" method="post" action="process.php">
+            <label for="event_name">Event name:</label>
+            <input type="text" name="event_name" placeholder="Navn på eventet"/>
+            <label for="description">Description</label>:
+            <textarea name="description">Beskrivelse...</textarea>
+            <label for="cost">Cost:</label>
+            <input type="number" name="cost" placeholder="Hvor mye koster det?"/>
+            <!--<label>Image</label>
+    <input type="image" name="image" />
+    <br>-->
+            <label for="date_time">When:</label>
+            <input type="datetime-local" name="date_time"/>
+            <label for="venue">Venue:</label>
+            <input type="text" name="venue" placeholder="Utested"/>
+            <input type="submit" value="Add event">
+        </form>
+    </div>
+</main>
 </body>
-
 </html>
