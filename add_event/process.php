0f601adc099e8b001860e0068134fa813f83333b
sveven <even.romo@gmail.com>
2017-05-11 13:13:00 +0200

    FINALYYYYYYYYYSYSYSYS add_event fungerer

--- add_event/process.php
+++ add_event/process.php
@@ -1,14 +1,15 @@
 <?php
 
 require '../config.php';
-error_log(print_r($_POST,true));
+error_log(print_r($_GET,true));
 
+    //var_dump($_GET);
 
-	$title = $_POST['event_name'];
-	$description = $_POST['description'];
-	$cost = $_POST['cost'];
-	$date_time = $_POST['date_time'];
-	$venue_id = $_POST['venue'];
+	$title = $_GET['event_name'];
+	$description = $_GET['description'];
+	$cost = $_GET['cost'];
+	$date_time = $_GET['date_time'];
+	$venue_id = $_GET['venue'];
 
 	try{
 		$sql='INSERT INTO events(title,description,cost,starts_at,venue_id) VALUES(:event_name,:description,:cost,:date_time,:venue)';
@@ -18,12 +19,11 @@ error_log(print_r($_POST,true));
 		$stmt->bindValue(':cost', $cost, PDO::PARAM_INT);
 		$stmt->bindValue(':date_time', $date_time, PDO::PARAM_STR);
 		$stmt->bindValue(':venue', $venue_id, PDO::PARAM_INT);
-        if (isset($stmt)) {
-            $stmt->execute(array(':event_name' => $title, ':description' => $description, ':cost' => $cost, ':starts_at' => $date_time, ':venue' => $venue_id));
-        }
-		//$stmt->execute();
+		$stmt->execute();
 		$affected_rows = $stmt->rowCount();
 	}
 	catch(PDOException $e) {
 		echo $e->getMessage();
 }
+
+header('Location: '.'add_event.php');
