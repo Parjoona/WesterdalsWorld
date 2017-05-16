c5d6f9cf59e5bba6841c5fd390ad8cfa3cf61f97
pauma <parjoona@hotmail.com>
2017-05-16 12:02:13 +0200

    Deleta zipfilen? Fixade på querys i config

--- config.php
+++ config.php
@@ -5,18 +5,14 @@ $db_pass = '';
 $db_name = 'westerproject';
 $db_port = 3306;
 
-$conn = new PDO('mysql:host=localhost;dbname=westerproject',$db_user,$db_pass);
-/*
+$conn = new PDO('mysql:host=localhost;dbname=westerproject', $db_user, $db_pass);
+/* Checkar om det blir errors, *INAKTIVERA FÖR DEN SKA LÄGGAS UT PÅ RIKTIG SIDA*
 try {
     $conn = new PDO('mysql:host=localhost;dbname=westerproject',$db_user,$db_pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $ex) {
     echo 'Not connected'.$ex->getMessage();
 }
-*/
-
-// Sätter igång databasen vid namn westerproject,
-// använder sig av kollonen places.
 
 //Checkar vilken sida du är på.
 $serv = $_SERVER['REQUEST_URI'];
@@ -29,7 +25,16 @@ if( strpos( $serv,'index') || strpos($serv,'place') !== false ) {
     $stmtevents->execute();
     $events = $stmtevents->fetchAll();
 }
-
+*/
+// Hämtar places
+$stmtplaces = $conn->prepare('SELECT * FROM places');
+$stmtplaces->execute();
+$places = $stmtplaces->fetchAll();
+// Hämtar events
+$stmtevents = $conn->prepare('SELECT * FROM events');
+$stmtevents->execute();
+$events = $stmtevents->fetchAll();
+// Hämtar message
 $stmtmsg = $conn->prepare('SELECT * FROM msg');
 $stmtmsg->execute();
 $msgs = $stmtmsg->fetchAll();
--- underpages/place-info.php
+++ underpages/place-info.php
@@ -16,15 +16,15 @@ foreach ($places as $place) {
 
         <!-- container för vänster information-->
         <div class="info-container">
+            <div class="info-img">
+                <?php echo "<img srcset=\"{$place['img_url']}\" src='imgbin/alt_place.png'/>"; ?>
+            </div>
             <div class="info-name"><?php echo $place['place_name']; ?></div>
             <div class="info-place">DISTANCE: <?php echo $place['distance']; ?></div>
             <div class="info-type">STED TYPE: <?php echo $place['place_type']; ?></div>
             <div class="info-address">ADRESS: <?php echo $place['place_adress']; ?></div>
             <div class="info-time">OPENED AT:
                 <?php echo $place['open_at']; ?>-<?php echo $place['closes_at']; ?></div>
-            <div class="info-img">
-                <?php echo "<img srcset=\"{$place['img_url']}\" src='imgbin/alt_place.png'/>"; ?>
-            </div>
             <div class="info-description"><?php echo $place['description']; ?></div>
         </div>
         <!-- Message window till höger -->
